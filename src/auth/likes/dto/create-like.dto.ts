bead6730be4f841ed9f98cac2d035a7782f20720
BrunoTruber <brunotruber45@gmail.com>
2021-10-14 14:53:36 -0300

    likes: controller and dto

--- src/auth/likes/dto/create-like.dto.ts
+++ src/auth/likes/dto/create-like.dto.ts
@@ -2,14 +2,14 @@
 import { IsNumber } from 'class-validator';
 
 export class CreateLikeDto {
-    @IsNumber()
-    Tweet: number;
+    // @IsNumber()
+    // Tweet: number;
 
     @IsNumber()
     tweetId: number;
 
-    @IsNumber()
-    User: number;
+    // @IsNumber()
+    // User: number;
 
     @IsNumber()
     userId: number;
--- src/auth/likes/likes.controller.ts
+++ src/auth/likes/likes.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('likes')
-export class LikesController {}
+/* eslint-disable prettier/prettier */
+import {
+    Body,
+    Controller,
+    Delete,
+    Param,
+    ParseIntPipe,
+    Post,
+    Req,
+    UseGuards,
+    UsePipes,
+    ValidationPipe,
+  } from '@nestjs/common';
+  import { CreateLikeDto } from 'src/auth/likes/dto/create-like.dto';
+  import { LikesService } from './likes.service';
+  import { Like } from '.prisma/client';
+  import { AuthGuard } from '@nestjs/passport';
+  
+  @Controller('likes')
+  export class LikesController {
+    constructor(private likeService: LikesService) {}
+  
+    @UseGuards(AuthGuard('jwt'))
+    @UsePipes(ValidationPipe)
+    @Post('likes')
+    async createLike(@Body() like: CreateLikeDto, @Req() req): Promise<Like> {
+      const user = req.user.id;
+      return this.likeService.postlike(like, user);
+    }
+  
+    @UseGuards(AuthGuard('jwt'))
+    @UsePipes(ValidationPipe)
+    @Delete('delete/:id')
+    async deletelike(@Param('id', ParseIntPipe) id: number,@Req() req,): Promise<Like> {
+      const user = req.user.id;
+  
+      return this.likeService.deslike(id, user);
+    }
+  }
+  
